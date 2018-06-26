a19c73c00ae2c9007f46024fe0e40471880b7385
Prince Kumar <prince.prakashsao@gmail.com>
2018-06-26 19:38:46 +0000

    added session, passport-twitter-authentication, get user's access keys

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var TwitterStrategy = require("passport-twitter");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
 
 //TWITTER
 var Twit = require("twit");
@@ -11,24 +11,86 @@ var config = require('./config');
 var T = new Twit(config);
 
 
+
+
+
+//SETUP
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("express-session")({
+    secret: "One-Direct Assignment Project",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//SETUP Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Configure stratergy
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "https://sit-a7princekumar.c9users.io/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    console.log("TOKEN: "+token);
+    console.log("TOKEN SECRET: "+tokenSecret);
+    console.log("--------------------------");
+    console.log(profile._json);
+    console.log("--------------------------");
+    return cb( null, profile );
+  }
+));
+
+
+
+
+
 //Routes
+//HOME
 app.get("/", function(req, res){
     res.render("home");
 });
 
+
+//Authenticates app to use users' data
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+
+//handle authentication-callback
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
+
+//handle user-input
 app.post("/", function(req, res){
     var twitter_user_id = req.body.twitter_user_id;
     console.log("Username: "+twitter_user_id);
     
-    T.get('followers/ids', { screen_name: twitter_user_id },  function (err, data, response) {
+    T.get('statuses/home_timeline', function(err, data, response){
         if(err){
             console.log(err.message);
-            console.log("Invalid username. Try a different one.");
         }else{
-            console.log(data);
+            // for(var i=0; i<data.length; i++){
+            //     console.log(data[i].text);       
+            // }
+            console.log("Timeline received.");
         }
     });
 });
+
+
+
+
+
 
 //listener
 app.listen(process.env.PORT, process.env.IP, function(){
