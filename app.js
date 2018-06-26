7ba8269023e516af74cb60728650bf662ece285d
Prince Kumar <prince.prakashsao@gmail.com>
2018-06-26 11:42:36 +0000

    getting user's twitter-id

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //Routes
 app.get("/", function(req, res){
     res.render("home");
+});
+
+app.post("/", function(req, res){
+    var twitter_user_id = req.body.twitter_user_id;
+    console.log(twitter_user_id);
 });
 
 //listener
