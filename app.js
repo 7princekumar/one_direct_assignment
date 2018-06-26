8236269d1f70b215cbc06b23253bf542353b7559
Prince Kumar <prince.prakashsao@gmail.com>
2018-06-26 12:55:08 +0000

    added twit(twitter) api

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//TWITTER
+var Twit = require("twit");
+var config = require('./config');
+var T = new Twit(config);
+
+
 //Routes
 app.get("/", function(req, res){
     res.render("home");
@@ -12,7 +18,16 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     var twitter_user_id = req.body.twitter_user_id;
-    console.log(twitter_user_id);
+    console.log("Username: "+twitter_user_id);
+    
+    T.get('followers/ids', { screen_name: twitter_user_id },  function (err, data, response) {
+        if(err){
+            console.log(err.message);
+            console.log("Invalid username. Try a different one.");
+        }else{
+            console.log(data);
+        }
+    });
 });
 
 //listener
